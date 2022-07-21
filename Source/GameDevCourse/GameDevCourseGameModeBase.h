df143b968cc5dfd09e3828ad230158076b628aa5
Pang-GJ <2362421192@qq.com>
2022-07-21 15:38:35 +0800

    Init commit

---
+++ Source/GameDevCourse/GameDevCourseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameDevCourseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEDEVCOURSE_API AGameDevCourseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
